1ae425531fe45dc0eb4c7fafcbb4db20d9a61b27
Ilya P <j@rtur.me>
2018-08-03 00:48:16 +0200

    FP in Scala up to 3.23

--- books/fpscala/src/main/scala/works/merely/fpscala/chapter3.scala
+++ books/fpscala/src/main/scala/works/merely/fpscala/chapter3.scala
@@ -70,4 +70,56 @@ object chapter3 {
 
   // I needed a hint to get this one, I had a feeling that it is possible but couldn't really get it all by myself.
   def foldLeftInFoldRight2[A, B](xs: List[A], z: B)(f: (B, A) => B): B = xs.foldRight { b: B => b }((x, b) => s => b(f(s, x)))(z)
+
+  // 3.14
+  def append[A](xs: List[A], ys: List[A]): List[A] = xs.foldRight(ys)((x, acc) => x :: acc)
+
+  // 3.15
+  def flatten[A](xss: List[List[A]]): List[A] = xss.foldRight(List[A]())(append)
+
+  // 3.16
+  def add1(xs: List[Int]): List[Int] = xs.foldRight(List[Int]())((x, acc) => (x + 1) :: acc)
+
+  // 3.17
+  def dtos(xs: List[Double]): List[String] = xs.foldRight(List[String]())((x, acc) => x.toString :: acc)
+
+  // 3.18
+  def map[A, B](xs: List[A])(f: A => B): List[B] = xs.foldRight(List[B]())((x, acc) => f(x) :: acc)
+
+  // 3.19
+  def filter[A](xs: List[A])(p: A => Boolean): List[A] = xs.foldRight(List[A]())((x, acc) => if (p(x)) x :: acc else acc)
+
+  // 3.20
+  def flatMap[A, B](xs: List[A])(f: A => List[B]): List[B] = flatten(xs.map(f))
+
+  // 3.21
+  def filterInFlatMap[A](xs: List[A])(p: A => Boolean): List[A] = flatMap(xs) { x => if (p(x)) List(x) else Nil }
+
+  // 3.22
+  def addTwoLists(xs: List[Int], ys: List[Int]): List[Int] = {
+    @tailrec
+    def loop(xs: List[Int], ys: List[Int], rs: List[Int]): List[Int] = {
+      (xs, ys) match {
+        case (Nil, _) => rs
+        case (_, Nil) => rs
+        case (x :: xs, y :: ys) => loop(xs, ys, (x + y) :: rs)
+      }
+    }
+
+    loop(xs, ys, List[Int]()).reverse
+  }
+
+  // 3.23
+  def zipWith[A, B, C](xs: List[A], ys: List[B])(f: A => B => C): List[C] = {
+    @tailrec
+    def loop(xs: List[A], ys: List[B], rs: List[C]): List[C] = {
+      (xs, ys) match {
+        case (Nil, _) => rs
+        case (_, Nil) => rs
+        case (x :: xs, y :: ys) => loop(xs, ys, f(x)(y) :: rs)
+      }
+    }
+
+    loop(xs, ys, List[C]()).reverse
+  }
 }
